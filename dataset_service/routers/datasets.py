fefa0a336fd12645fa9fe8d3b88aaf42631fcf94
agrieby-chaniago <agrieby.chaniago@student.uhb.ac.id>
2026-06-14 19:23:20 +0700

    lalala yeyeye

--- dataset_service/routers/datasets.py
+++ dataset_service/routers/datasets.py
@@ -8,7 +8,7 @@ router = APIRouter()
 # DELETE /api/v1/datasets/{id}   → soft delete (set status = "deleted")
 #
 # Gunakan:
-#   from dataset_service.models.database import get_db
-#   from dataset_service.models.orm import Dataset, DatasetClass
-#   from dataset_service.models.schemas import DatasetSchema, DatasetDetailSchema
+#   from models.database import get_db
+#   from models.orm import Dataset, DatasetClass
+#   from models.schemas import DatasetSchema, DatasetDetailSchema
 #   from shared.response import success_response, error_response
